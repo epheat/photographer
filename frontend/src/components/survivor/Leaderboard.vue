0396f71d8fba45cff9749e531af06329861416a7
Evan Heaton <epheat07+github@gmail.com>
2022-03-16 23:44:52 -0700

    spruce up the leaderboard

--- frontend/src/components/survivor/Leaderboard.vue
+++ frontend/src/components/survivor/Leaderboard.vue
@@ -5,13 +5,11 @@
         <th>place</th>
         <th>username</th>
         <th>points</th>
-        <th>change</th>
       </tr>
       <tr v-for="row in data" :key="row.resourceId">
-        <td>{{ row.placement }}</td>
+        <td><div class="place"><div>{{ row.placement }}</div><i class="medal" v-if="row.placement <= 3" :class="medalClass(row)"></i><i class="arrow" :class="changeClass(row)"></i></div></td>
         <td>{{ row.username }}</td>
         <td>{{ row.points }}</td>
-        <td class="center">{{ getChange(row) }}</td>
       </tr>
     </table>
   </div>
@@ -24,20 +22,31 @@ export default {
     data: Array,
   },
   methods: {
-    getChange(row) {
+    changeClass(row) {
       const mostRecentEvent = row.placementHistory.find(placementEvent =>
           placementEvent.timestamp === row.lastUpdatedDate).event;
-      const mostRecentEpisode = mostRecentEvent.split("-")[1];
+      const mostRecentEpisode = mostRecentEvent.split("-")[3];
       const placements = row.placementHistory.filter(placementEvent => !placementEvent.event.includes(mostRecentEpisode))
-                                             .sort((a, b) => b.timestamp - a.timestamp);
+          .sort((a, b) => b.timestamp - a.timestamp);
       if (placements.length > 0) {
         if (placements[0].placement > row.placement) {
-          return "⬆"
+          return { arrow: true, up: true }
+        } else if (placements[0].placement < row.placement) {
+          return { arrow: true, down: true }
         } else {
-          return "⬇"
+          return { line: true };
         }
       } else {
-        return "-";
+        return { line: true };
+      }
+    },
+    medalClass(row) {
+      if (row.placement === 1) {
+        return { gold: true };
+      } else if (row.placement === 2) {
+        return { silver: true };
+      } else {
+        return { bronze: true };
       }
     }
   }
@@ -65,6 +74,54 @@ export default {
 
   tr:nth-child(even) {
     background-color: $ps-lightest-grey;
+  }
+  .place {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  // https://www.w3schools.com/howto/howto_css_arrows.asp
+  .arrow {
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    align-self: end;
+
+    &.up {
+      border: solid $ps-button-green;
+      border-width: 0 3px 3px 0;
+      transform: rotate(-135deg);
+    }
+    &.down {
+      border: solid $ps-button-red-shadow;
+      border-width: 0 3px 3px 0;
+      transform: translateY(-6px) rotate(45deg);
+    }
+  }
+  .line {
+    border: solid #5e5e5e;
+    border-width: 3px 0 0 0;
+    display: inline-block;
+    padding: 4px;
+    transform: translateY(3px);
+  }
+
+  .medal {
+    width: 12px;
+    height: 12px;
+    border-radius: 12px;
+    box-shadow: 0px 1px 1px #444;
+
+    &.gold {
+      background-color: goldenrod;
+    }
+    &.silver {
+      background-color: silver;
+    }
+    &.bronze {
+      background-color: sienna;
+    }
   }
 }
 </style>
