9b092684c2afba68520ddfda547019fd200a14c8
Evan Heaton <epheat07+github@gmail.com>
2023-09-06 00:51:50 -0700

    Add some buttons with press animations

--- frontend/src/phaser/battletd/gameobjects/UIButton.ts
+++ frontend/src/phaser/battletd/gameobjects/UIButton.ts
@@ -6,10 +6,15 @@ export interface ButtonConfig {
 }
 
 export class UIButton extends Phaser.GameObjects.Container {
+  private onClick?: Function;
+  private pressed: boolean = false;
+  private sprite: Phaser.GameObjects.NineSlice;
+
   constructor(scene: Phaser.Scene, x: number, y: number, buttonConfig: ButtonConfig) {
     super(scene, x, y);
-    const button = scene.add.nineslice(0, 0, buttonConfig.texture ?? 'button_flat_small', 0, buttonConfig.width ?? 64, 32, 8, 8, 8, 8)
-    this.add(button);
+    this.sprite = scene.add.nineslice(0, 0, buttonConfig.texture ?? 'button_small', 'BTD_button_small.png', buttonConfig.width ?? 64, 32, 8, 8, 8, 8)
+    this.add(this.sprite);
+    this.onClick = buttonConfig.onClick;
     if (buttonConfig.text) {
       const text = scene.add.text(0, 0, buttonConfig.text, {
         color: "#222",
@@ -18,10 +23,35 @@ export class UIButton extends Phaser.GameObjects.Container {
       this.add(text);
     }
     this.scene.add.existing(this);
-    button.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, buttonConfig.onClick ?? this.onClick);
+    this.sprite.setInteractive()
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.onUp.bind(this))
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, this.onDown.bind(this))
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, this.onOver.bind(this))
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, this.onOut.bind(this))
   }
 
-  onClick() {
-    console.log(`clicked on button ${this.name}`);
+  clearAlpha(): this {
+    return super.clearAlpha();
+  }
+
+  onUp() {
+    this.pressed = false;
+    this.sprite.setFrame('BTD_button_small.png');
+    this.onClick?.();
+  }
+
+  onDown() {
+    this.pressed = true;
+    this.sprite.setFrame( 'BTD_button_small_press.png');
+  }
+
+  onOver() {
+    if (this.pressed) {
+      this.sprite.setFrame( 'BTD_button_small_press.png');
+    }
+  }
+
+  onOut() {
+    this.sprite.setFrame( 'BTD_button_small.png');
   }
 }
--- frontend/src/phaser/battletd/scenes/GameUI.ts
+++ frontend/src/phaser/battletd/scenes/GameUI.ts
@@ -1,4 +1,5 @@
-import buttonFlatSmall from "@/phaser/battletd/assets/ui/Complete_GUI_Essential_Pack_Free_Version/01_Basic_Collection/01_Flat_Theme/UI_Flat_Button_Small.png";
+import buttons from "@/phaser/battletd/assets/ui/BTD_button_atlas.png";
+import buttonsAtlas from "@/phaser/battletd/assets/ui/BTD_button_atlas.json";
 import { UIButton } from "@/phaser/battletd/gameobjects/UIButton";
 import { eventBus, events } from "@/phaser/battletd/events/EventBus";
 
@@ -14,7 +15,7 @@ export default class GameUI extends Phaser.Scene {
 
 
   preload() {
-    this.load.image('button_flat_small', buttonFlatSmall);
+    this.load.atlas('button_small', buttons, buttonsAtlas);
   }
 
   create() {
